b3614fa038df2a1de44dc105cd706f014dc228a4
lderville <91890830+lderville@users.noreply.github.com>
2022-01-21 10:34:19 +0100

    connexion

--- 4-accesDonnees/tp1/tp1cnx.php
+++ 4-accesDonnees/tp1/tp1cnx.php
@@ -1,7 +1,7 @@
 <?php
 try {
-    $chaineCnx = 'mysql:host=localhost;dbname=tp';
-    $pdo = new PDO($chaineCnx, 'root', '', [PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"]);
+    $chaineCnx = 'mysql:host=bpbukh41fqvikukr5pt2-mysql.services.clever-cloud.com;dbname=bpbukh41fqvikukr5pt2';
+    $pdo = new PDO($chaineCnx, 'ucitw2mcqnjtdn6b', 'w82VHv9qqotSYVNh4zXy', [PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"]);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
     $msg = 'ERREUR PDO dans ' . $e->getFile() . ' : ' . $e->getLine() . ' : ' . $e->getMessage();
